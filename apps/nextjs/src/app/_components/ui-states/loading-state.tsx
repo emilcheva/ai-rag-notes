d8bcac9401301df998a86d063cb731242630e80e
emilcheva <evgenia.milcheva@hotmail.com>
2025-07-28 14:53:07 +0300

    chore: update styles for other visual states

--- apps/nextjs/src/app/_components/ui-states/loading-state.tsx
+++ apps/nextjs/src/app/_components/ui-states/loading-state.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const LoadingState = ({ title, description }: Props) => {
   return (
     <div className="flex flex-1 items-center justify-center px-8 py-4">
-      <div className="flex flex-col items-center justify-center gap-y-6 rounded-lg bg-background p-10 shadow-sm">
+      <div className="flex flex-col items-center justify-center gap-y-6 rounded-xl border-2 border-dashed bg-background p-10 shadow-sm">
         <Loader2Icon className="size-6 animate-spin text-primary" />
         <div className="flex flex-col gap-y-2 text-center">
           <h6 className="text-lg font-medium">{title}</h6>
